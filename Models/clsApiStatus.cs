56c40010c0331b07a9bc6c30be51bc8eb00d1dfa
Denisecl1 <denisecamp21@gmail.com>
2025-09-27 16:02:57 -0600

    Primera version

---
+++ Models/clsApiStatus.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apiRESTCheckBdTADS.Models
+{
+    //modelo de saida de la API
+    public class clsApiStatus
+    {
+        // Estado de ejecución del endpoint (método)
+        public bool statusExec { get; set; }
+        // Descripción del resultado de tipo boleano si o no 
+        public string msg { get; set; }
+        // Código de ejecución del endpoint (método) mensaje al usuario
+        public int ban { get; set; }
+        // Objeto Json para envío de datos status de salida del mensaje 
+        public JObject datos { get; set; }
+    }
+}
---
+++ Models/clsCheckBd.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using apiRESTCheckBdTADS.Models;    
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;  
+using System.Configuration;
+
+namespace apiRESTCheckBdTADS.Models
+{
+    public class clsCheckBd
+    {
+        //Definicion del atributo privado Conexion
+        private String cadConn = ConfigurationManager.ConnectionStrings["bdControlAcceso"].ConnectionString;
+        //definicion de atributos de salida
+        public string statusMsg;
+        public int ban;
+
+        //Definicion del metododo de conexion a MySQL
+        public void checkBd()
+        {
+            try
+            {
+               MySqlConnection conn = new MySqlConnection(cadConn);
+                conn.Open();
+                conn.Close();
+                // conexion exitosa , enviar salida
+                ban = 1;
+                statusMsg = "Conexión exitosa a la base de datos.";
+            }
+            catch (Exception ex)
+            {
+                // error de conexion , enviar salida
+                ban = 0;
+                statusMsg = ex.Message.ToString();
+
+            }
+
+        }
+    }
+}
